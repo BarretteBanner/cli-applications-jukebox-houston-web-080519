0371bf9271718aa18e4b653be660d9ba9fbba7af
Barrette Banner <barrettebanner@gmail.com>
2019-07-25 19:43:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,7 @@ def play(songs)
   input = gets.chomp 
   if songs.include?(input)
     puts "Playing #{input}"
-  elsif (1..0).to_a.include?(input.to_i)
+  elsif (1..9).to_a.include?(input.to_i)
     puts "Playing #{songs[input.to_i - 1]}"
   else
     puts "Invalid input, please try again"
